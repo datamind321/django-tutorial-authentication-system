08082b8010da85d5c6323cb75145f48523492241
Data-Mind 2.0 <112772762+datamind321@users.noreply.github.com>
2023-12-16 13:51:57 +0530

    Update settings.py

--- djoser authentication/auth/auth/settings.py
+++ djoser authentication/auth/auth/settings.py
@@ -222,5 +222,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sunnydada550@gmail.com'
-EMAIL_HOST_PASSWORD = 'prom pmfd umug eqly'
+EMAIL_HOST_USER = 'enter your development testing email'
+EMAIL_HOST_PASSWORD = 'enter your email app password'
